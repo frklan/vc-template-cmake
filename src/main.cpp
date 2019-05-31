ccee83acff6d971ad620ffee50e774ca5368d2cd
Fredrik Andersson <fredrik.k.andersson@gmail.com>
2019-05-31 21:50:42 +0200

    Added auto incremeting build string

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
+#include "version.h"
 #include "vct/vct.h"
 
 int main() {
   std::clog << "--- vc-template-cmake ---\n";
+  std::clog << "Build version: " << version() << '\n';
   
   std::clog << "Hello world!\n" << vct() << '\n';
   
---
+++ src/version.h
@@ -0,0 +1,5 @@
+// brief Header file for version number function
+// (C) Wolfram Rösler <wolfram@roesler-ac.de>
+
+
+char const* version();
